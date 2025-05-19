8b61097b99b530c106430a36d96cadf29c03d313
ThomasFerl <thomasferl@gmail.com>
2025-05-19 17:10:58 +0200

    backendGlobals

--- banf/backendGlobals.js
+++ banf/backendGlobals.js
@@ -1,4 +1,10 @@
-module.exports.staticPath        = '';
-module.exports.symbolPath        = () => {return this.staticPath + '/tfWebApp/symbols';};
-module.exports.ignoreSession     = true;
-module.exports.isMediaCollector  = true;
+const path          = require('path');
+
+var   ___staticPath = '';
+
+module.exports.staticPath           = (pathName) => {if(pathName) ___staticPath = pathName; return ___staticPath;};
+module.exports.symbolPath           = () => {return path.join(___staticPath , 'tfWebApp' , 'symbols');};
+module.exports.ignoreSession        = true;
+
+module.exports.__Port_webSocket     =  4444;
+module.exports.maxAgePayloadHistory = 31;  // Tage
